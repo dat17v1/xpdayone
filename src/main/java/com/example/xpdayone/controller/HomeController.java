76cfbda5859e18a15263f0b9e697c7834ef4a6bf
joneikholm <joneikholm@gmail.com>
2018-03-09 09:27:24 +0100

    Session login

--- src/main/java/com/example/xpdayone/controller/HomeController.java
+++ src/main/java/com/example/xpdayone/controller/HomeController.java
@@ -3,12 +3,17 @@ package com.example.xpdayone.controller;
 
 import com.example.xpdayone.businesslogic.StatsCalculator;
 import com.example.xpdayone.model.Database;
+import com.example.xpdayone.model.Login;
 import com.example.xpdayone.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -18,19 +23,46 @@ public class HomeController {
     private List<User> users = database.getAllUsers();
     private StatsCalculator statsCalculator = new StatsCalculator(database);
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Model model){
-        model.addAttribute("userlist", users);
+    @ModelAttribute
+    public void addUsers(Model model){
+        model.addAttribute("userlist", database.getAllUsers());
         model.addAttribute("average", statsCalculator.getAverageAmount());
+
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model, HttpSession session){
+        //session.setAttribute("status", "1"); // leger at vi har logget på
+        System.out.println("Session id:" + session.getId());
         return "index";
     }
+
+
 
     @RequestMapping(value = "/addUser", method = RequestMethod.POST)
     public String addUser(User user, Model model){
         System.out.println("tilføjet user: " + user.getName());
         addNewUser(user);
-        model.addAttribute("userlist", users);
-        model.addAttribute("average", statsCalculator.getAverageAmount());
+
+//        model.addAttribute("userlist", database.getAllUsers());
+//        model.addAttribute("average", statsCalculator.getAverageAmount());
+        return "index";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(Login login, Model model, HttpSession session){
+        if(login.getName().equals("anna") && login.getPassword().equals("123456")){
+            session.setAttribute("status", "1");
+            System.out.println("Logget på");
+        }else {
+            session.setAttribute("status", "0");
+            System.out.println("Ikke logget på");
+        }
+//        try {
+//            response.sendRedirect("/");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         return "index";
     }
 
---
+++ src/test/java/com/example/xpdayone/businesslogic/StatsCalculatorTest.java
@@ -0,0 +1,41 @@
+package com.example.xpdayone.businesslogic;
+
+import com.example.xpdayone.model.Database;
+import com.example.xpdayone.model.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+public class StatsCalculatorTest {
+
+    StatsCalculator statsCalculator; // code to test
+
+    @Mock
+    Database database;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this); // Spring
+        statsCalculator = new StatsCalculator(database);
+    }
+
+    @Test
+    public void getAverageAmount() {
+        // Lav test fixture:
+        List<User> users = new ArrayList<>();
+        users.add(new User(1,"", 123));
+        users.add(new User(1,"", 124));
+        users.add(new User(1,"", 125));
+        when(database.getAllUsers()).thenReturn(users);
+        assertEquals(122, statsCalculator.getAverageAmount(), 0.0);
+
+
+    }
+}
